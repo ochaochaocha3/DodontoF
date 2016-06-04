5bf3ce5dc023b05e7a8fd3b4d64652216802d05d
ocha <ochaochaocha3@gmail.com>
2016-06-04 16:30:49 +0900

    AssertHaveKeys: 詳細をログに表示させる

--- test_ruby/test/unit/assertions/assert_have_keys.rb
+++ test_ruby/test/unit/assertions/assert_have_keys.rb
@@ -6,7 +6,7 @@ module Test::Unit::Assertions
   # +hash+ のキーに +keys+ がすべて存在することを表明する
   def assert_have_keys(hash, *keys)
     keys.each do |k|
-      assert_equal(true, hash.has_key?(k))
+      assert_equal(true, hash.has_key?(k), %Q(キー "#{k}" が存在する))
     end
   end
 end
